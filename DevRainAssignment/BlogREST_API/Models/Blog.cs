03f8a13123a09723407b4b89daf3eff933234283
Yaroslav Kolisnyk <kolisnukyaroslav77@gmail.com>
2020-10-20 23:07:52 +0300

    Add models

---
+++ DevRainAssignment/BlogREST_API/Models/Blog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogREST_API.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Name { get; set;}
+
+        [Required]
+        [MaxLength(250)]
+        public string Topic { get; set; }
+
+        [Required]        
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime Published{ get; set; }
+
+        public ICollection<Comment> Comments { get; set;}
+    }
+}
---
+++ DevRainAssignment/BlogREST_API/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogREST_API.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string UserName { get; set; }        
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime Published { get; set; }
+
+        public Blog Blog { get; set; }
+    }
+}
