cd7ac7ee0d03808574b80705293f884cab5a89eb
Yaroslav Kolisnyk <kolisnukyaroslav77@gmail.com>
2020-10-27 01:28:36 +0200

    Add test project ; module and integration tests

---
+++ DevRainAssignment/BlogAPI.Test/BlogTest.cs
@@ -0,0 +1,22 @@
+﻿using BlogREST_API.Interfaces;
+using BlogREST_API.Models;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BlogAPI.Test
+{
+    public class BlogTest
+    {
+        
+        [Fact]
+        public void MethodShouldReturnNotNull()
+        {
+            var mockRepository = new Mock<IBlogRepository>();
+            mockRepository.Setup(a => a.GetAllItems()).Returns(new List<Blog>());
+            var controller = mockRepository.Object;
+
+            Assert.NotNull(controller);
+        }
+    }
+}
---
+++ DevRainAssignment/BlogAPI.Test/IntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BlogAPI.Test
+{
+    public class IntegrationTest: IClassFixture<WebApplicationFactory<BlogREST_API.Startup>>
+    {
+        private readonly WebApplicationFactory<BlogREST_API.Startup> _factory;
+
+        public IntegrationTest(WebApplicationFactory<BlogREST_API.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/blogs")]
+        [InlineData("/api/blogs/2")]
+        [InlineData("/api/blogs/1/comments")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); 
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
